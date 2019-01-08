86cc66afab21419053ed8cf05013f568eb5a849a
Justin Lee <justinlee0022@gmail.com>
2019-01-08 08:36:07 -0800

    Added css and style loader, working on basic grid layout for graph feedback

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,8 @@ module.exports = {
   },
   module: {
     rules: [
-      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ }
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] }
     ]
   },
   plugins: [HtmlWebpackPluginConfig]
