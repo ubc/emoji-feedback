8c3116672bf3ac32c7753e26affbc40b791d7dcd
Justin Lee <justinlee0022@gmail.com>
2019-01-14 10:23:32 -0800

    Cleaned up index.js

--- src/index.js
+++ src/index.js
@@ -2,17 +2,8 @@ import css from './index.css'
 import controller from './app'
 
 const app = controller()
-// const app1 = controller()
 
 const emojis = [
-  { emojicon: '😀', emotion: 'happy' },
-  { emojicon: '😞', emotion: 'sad' },
-  { emojicon: '😕', emotion: 'confused' },
-  { emojicon: '👍', emotion: 'like' },
-  { emojicon: '👎', emotion: 'dislike' }
-]
-
-const emojis1 = [
   { emojicon: '😁', emotion: 'superhappy' },
   { emojicon: '😀', emotion: 'happy' },
   { emojicon: '😐', emotion: 'indifferent' },
@@ -22,20 +13,10 @@ const emojis1 = [
 
 app.init({
   entryId: 'entry',
-  emojis: emojis1,
+  emojis: emojis,
   endpoints: {
     emojiEndpoint: 'http://localhost:8080/emoji',
     formEndpoint: 'http://localhost:8080/form',
     votesEndpoint: 'http://localhost:8080/votes'
   }
 })
-
-// app1.init({
-//   entryId: 'entry1',
-//   emojis,
-//   endpoints: {
-//     emojiEndpoint: 'http://localhost:8080/',
-//     formEndpoint: 'http://localhost:8080/',
-//     votesEndpoint: 'http://localhost:8080/'
-//   }
-// })
