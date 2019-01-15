be798f0739269d0b59c4ef353cc973686532038d
Justin Lee <justinlee0022@gmail.com>
2019-01-15 10:39:01 -0800

    Added emoji button ui tests

--- test/ui.test.js
+++ test/ui.test.js
@@ -1,4 +1,4 @@
-/* global describe, test, beforeAll, afterAll */
+/* global describe, test, beforeAll, afterAll, expect */
 
 import puppeteer from 'puppeteer'
 
@@ -12,7 +12,7 @@ beforeAll(async () => {
   const height = 1080
   browser = await puppeteer.launch({
     headless: false,
-    slowMo: 80
+    slowMo: 300
   })
   page = await browser.newPage()
   await page.setViewport({ width, height })
@@ -22,9 +22,23 @@ afterAll(() => {
   browser.close()
 })
 
-describe('Buttons', () => {
+describe('Emoji buttons', () => {
   test('exist', async () => {
     await page.goto(APP)
-    await page.waitForSelector
+    let buttonIds = ['entry-superhappy', 'entry-happy', 'entry-indifferent', 'entry-unhappy', 'entry-disappointed']
+    const buttons = await page.evaluate(buttonIds => {
+      return buttonIds.map(id => document.getElementById(id).id)
+    }, buttonIds)
+    expect(buttons.length).toBe(5)
+    expect(buttons).toEqual(buttonIds)
+  })
+  test('on click, background turns blue and form appears', async () => {
+    await page.goto(APP)
+    const happyButton = await page.$('#entry-superhappy')
+    let className = await page.evaluate(button => button.getAttribute('class'), happyButton)
+    expect(className).toBe('button')
+    await happyButton.click()
+    className = await page.evaluate(button => button.getAttribute('class'), happyButton)
+    expect(className).toBe('button active')
   })
 })
